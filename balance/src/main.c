823fc1bb345e5a9de9382ccc603688c5ce122c2d
Evan <robotevan24@gmail.com>
2021-02-25 23:53:48 -0500

    adding test mpu read

--- balance/src/main.c
+++ balance/src/main.c
@@ -1,12 +1,15 @@
 #include "MotorDriver.h"
+#include "MPU.h"
 #include <unistd.h>
 
 
 void app_main() {
-
+    float temp;
+    init_mpu_6050();
     init_mcpwm(4,5,18,19);
     while (1){
-        printf("hello\n");
+        read_temperature(&temp);
+        printf("temperature is %f\n", temp);
     }
 }
 
